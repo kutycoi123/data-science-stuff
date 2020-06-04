b62922f6d9b3eaaf87366d138f16fa0d08aa6572
kutycoi123 <kutycoi123@gmail.com>
2020-06-04 14:52:58 -0700

    Fixed parse_dates

--- e3/smooth_temperature.py
+++ e3/smooth_temperature.py
@@ -5,23 +5,16 @@ from statsmodels.nonparametric.smoothers_lowess import lowess
 from pykalman import KalmanFilter
 import sys
 
-def to_datetime(str):
-    """Convert str to python datetime"""
-    return pd.Timestamp(str).to_pydatetime()
-
 def main():
     filename = sys.argv[1]
-    cpu_data = pd.read_csv(filename)
-    cpu_data['timestamp'] = cpu_data.apply(lambda x: to_datetime(x['timestamp']), axis=1)
+    cpu_data = pd.read_csv(filename, parse_dates=['timestamp'])
     # Loess
     loess_smoothed = lowess(cpu_data['temperature'], cpu_data['timestamp'], frac=0.02)
     # Kalman
     kalman_data = cpu_data[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
-
     initial_state = kalman_data.iloc[0]
-
     observation_covariance = np.diag([0.6,  1,    0.3    ,60]) ** 2
-    transition_covariance = np.diag( [0.01, 0.01, 0.03  ,7]) ** 2
+    transition_covariance = np.diag( [0.01, 0.01, 0.03   ,7])  ** 2
     transition_matrix = [[0.97,0.5,0.2,-0.001],[0.1,0.4,2.2,0],[0,0,0.95,0],[0,0,0,1]]
     kf = KalmanFilter(
         initial_state_mean=initial_state,
