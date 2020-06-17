4aa1559747dc6baab34892a09da6a890cebffa14
tln3 <tln3@sfu.ca>
2020-06-17 16:11:04 -0700

    Added generate random arrays and run all sort algo

---
+++ e6/create_data.py
@@ -0,0 +1,35 @@
+import sys
+import time
+import numpy as np
+import pandas as pd
+from implementations import all_implementations
+
+N_ARRAYS = 1000
+N_ELEM = 1000
+MAX_ELEM = 100
+
+def gen_random_arrays():
+    np.random.seed(1)
+    arrs = []
+    for _ in range(N_ARRAYS):
+        tmp = []
+        for _ in range(N_ELEM):
+            tmp.append(np.random.randint(MAX_ELEM))
+        arrs.append(tmp)
+    return np.array(arrs)
+
+def main():
+    rand_arrs = gen_random_arrays()
+    data = {'qs1':[],'qs2':[],'qs3':[],'qs4':[],'qs5':[],
+            'merge':[],'partition_sort':[]}
+    sort_names = ['qs1','qs2','qs3','qs4','qs5','merge','partition_sort']
+    for rand_arr in rand_arrs:
+        for idx,sort in enumerate(all_implementations):
+            start = time.time()
+            res = sort(rand_arr)
+            end = time.time()
+            data[sort_names[idx]].append(end-start)
+    data_df = pd.DataFrame(data)
+    print(data_df)
+if __name__ == "__main__":
+    main()
